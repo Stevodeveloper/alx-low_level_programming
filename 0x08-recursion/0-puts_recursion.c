7c35646cdb753c58c48211d3904be7738fdbec6b
Stevodeveloper <stepheneoduor20@gmail.com>
2022-06-22 00:54:06 -0700

    print a dunction

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: is a string
+ * Return: nothing in particular
+ */
+void _puts_recursion(char *s)
+{
+	s = "Puts with recursion";
+	_putchar(s);
+}
+
