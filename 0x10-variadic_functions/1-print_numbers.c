3b3ea2173a00103429bf1e753db8d84dea3180c6
Stevodeveloper <stepheneoduor20@gmail.com>
2022-07-07 03:39:04 -0700

    list of comma separated numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: is the separator to separate the numbers printed
+ * @n: the count of numbers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list argptr;
+
+	unsigned int count;
+
+	va_start(argptr, n);
+
+	for (count = 0; count < n; count++)
+	{
+		if (separator != NULL && count != n - 1)
+			printf("%u%s", va_arg(argptr, int), separator);
+		else
+			printf("%u", va_arg(argptr, int));
+	}
+	printf("\n");
+	va_end(argptr);
+}
+
