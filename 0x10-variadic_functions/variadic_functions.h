a4f5ff9b118d58d57e09b8a20e570186f986956d
Stevodeveloper <stepheneoduor20@gmail.com>
2022-07-07 03:10:37 -0700

    return sum of all args

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,8 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 
 #include <stdio.h>
 #include <stdarg.h>
+
+#endif /*VARIADIC_FUNCTIONS_H*/
