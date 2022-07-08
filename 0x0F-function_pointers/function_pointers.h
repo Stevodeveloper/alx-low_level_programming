c20f137c25ff405fd5d06c06d1a9744561ade047
Stevodeveloper <stepheneoduor20@gmail.com>
2022-07-08 00:41:48 -0700

    executes a function given as a parameter

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,5 +5,6 @@ void print_name(char *name, void (*f)(char *));
 #include <stdio.h>
 #include <stdlib.h>
 void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
